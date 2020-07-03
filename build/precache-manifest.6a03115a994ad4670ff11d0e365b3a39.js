c2f491453af8210046c363a4779c7310ae3ae01d
AnhTuan4198 <whitejunh@gmail.com>
2020-07-03 13:15:52 +0700

    make it better

---
+++ build/precache-manifest.6a03115a994ad4670ff11d0e365b3a39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8b463ef83cadc8b3f20be25cdbf594",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e472021338aaa8d9dceb",
+    "url": "/static/css/main.3b7aef88.chunk.css"
+  },
+  {
+    "revision": "73c929a680abc4f8e89f",
+    "url": "/static/js/2.bb33c4e4.chunk.js"
+  },
+  {
+    "revision": "73aed684990cc75483f1462d2606ca6f",
+    "url": "/static/js/2.bb33c4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e472021338aaa8d9dceb",
+    "url": "/static/js/main.accf9ac8.chunk.js"
+  },
+  {
+    "revision": "0105d2243e5e99f569b9",
+    "url": "/static/js/runtime-main.e3e055eb.js"
+  }
+]);
